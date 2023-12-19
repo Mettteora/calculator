5844c52c76f6f423f3ba7e46a08a9756e83eb64e
Mettteora <mrhens2013@gmail.com>
2023-12-19 16:20:59 +0100

    Finish

--- calc.js
+++ calc.js
@@ -25,8 +25,60 @@ document.querySelector(".buttons").addEventListener("click", (event) => {
     clearAll();
   }
 
-  out.textContent = ``;
+  // out.textContent = ``;
 
   const key = event.target.textContent;
   console.log(key);
+
+  if (digit.includes(key)) {
+    if (b === "" && sign === ``) {
+      a += key;
+
+      out.textContent = a;
+    } else if (a !== `` && b !== `` && finish) {
+      b = key;
+      finish = false;
+      out.textContent = b;
+    } else {
+      b += key;
+      out.textContent = b;
+    }
+    console.log(a, b, sign);
+    return;
+  }
+
+  if (action.includes(key)) {
+    sign = key;
+    out.textContent = sign;
+    console.table(a, b, sign);
+    return;
+  }
+
+  if (key === `=`) {
+    if (b === ``) b = a;
+    switch (sign) {
+      case `+`:
+        a = +a + +b;
+        break;
+      case `-`:
+        a = a - b;
+        break;
+      case `X`:
+        a = a * b;
+        break;
+      case `/`:
+        if (b === `0`) {
+          out.textContent = `Error`;
+          a = ``;
+          b = ``;
+          sign = ``;
+          return;
+        }
+        a = a / b;
+        break;
+    }
+    finish = true;
+    out.textContent = a;
+    console.table(a, b, sign);
+  }
 });
